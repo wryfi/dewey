a95decba68e3361e0b2cbff9d6689337572b2a2c
Chris Haumesser <chaumesser@plos.org>
2015-10-19 11:41:09 -0700

    fix bug in asset sync

--- modules/hardware/management/commands/sync_assets.py
+++ modules/hardware/management/commands/sync_assets.py
@@ -11,38 +11,37 @@ from hardware.models import Server
 
 class Command(BaseCommand):
     def _get_field(self, name, fields):
+        ''' Always returns a list, even when there is only one value'''
         for field in fields:
             if field['title'] == name:
                 return field['values']
 
     def _get_auth(self):
-        if settings.JIRA_USERNAME == '':
+        jira_user = settings.JIRA_USERNAME
+        jira_password = settings.JIRA_PASSWORD
+        if jira_user == '':
             jira_user = input('Jira Username: ')
-        else:
-            jira_user = settings.JIRA_USERNAME
-        if settings.JIRA_PASSWORD == '':
-           jira_password = getpass('Jira Password: ')
-        else:
-            jira_password = settings.JIRA_PASSWORD
+        if jira_password == '':
+            jira_password = getpass('Jira Password: ')
         return HTTPBasicAuth(jira_user, jira_password)
 
     def handle(self, *args, **options):
         url = '/'.join([settings.JIRA_URL, 'rest', 'com-spartez-ephor', '1.0', 'search'])
         auth = self._get_auth()
-        print(auth)
         payload = {'query': 'Category=Servers'}
         request = requests.get(url, auth=auth, params=payload)
         request.raise_for_status()
         assets = [item for item in request.json()['items'] if item['typeName'] == 'Computer']
         for asset in assets:
             status = self._get_field('Asset Status', asset['fields'])
-            if status[0] == 'Deployed':
-              try:
-                  server = Server.objects.get(ephor_id=asset['id'])
-              except Server.DoesNotExist:
-                  server = Server(ephor_id=asset['id'])
-              server.asset_tag = self._get_field('Title', asset['fields'])[0]
-              server.manufacturer = self._get_field('Manufacturer', asset['fields'])[0]
-              server.model = self._get_field('Model', asset['fields'])[0]
-              server.serial = self._get_field('Serial Number', asset['fields'])[0]
-              server.save()
+            if len(status) > 0 and status[0] == 'Deployed':
+                if self._get_field('Title', asset['fields'])[0] != 'TBD':
+                    try:
+                        server = Server.objects.get(ephor_id=asset['id'])
+                    except Server.DoesNotExist:
+                        server = Server(ephor_id=asset['id'])
+                    server.asset_tag = self._get_field('Title', asset['fields'])[0]
+                    server.manufacturer = self._get_field('Manufacturer', asset['fields'])[0]
+                    server.model = self._get_field('Model', asset['fields'])[0]
+                    server.serial = self._get_field('Serial Number', asset['fields'])[0]
+                    server.save()
