6af2f900a1604ccddfbaf32bd95d24afb2507fee
Chris Haumesser <chaumesser@plos.org>
2016-05-11 16:30:05 -0700

    ITP-833 initial config for using jsonapi

--- modules/dewey/settings/common.py
+++ modules/dewey/settings/common.py
@@ -157,6 +157,24 @@ LOGGING = {
 }
 
 
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 25,
+    'EXCEPTION_HANDLER': 'rest_framework_json_api.exceptions.exception_handler',
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework_json_api.pagination.PageNumberPagination',
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework_json_api.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser'
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework_json_api.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+    'DEFAULT_METADATA_CLASS': 'rest_framework_json_api.metadata.JSONAPIMetadata',
+}
+
+
 # Jira integration for syncing assets
 
 JIRA_USERNAME = get_env('JIRA_USERNAME')
