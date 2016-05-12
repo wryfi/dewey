76aad090bc9f6561093a6eafbdad5dd746b24711
Chris Haumesser <chaumesser@plos.org>
2016-05-12 13:59:12 -0700

    additiona pieces needed to fix this branch

--- modules/hardware/views.py
+++ modules/hardware/views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-from .models import Server
-from .serializers import ServerDetailSerializer, ServerListSerializer
+from .models import NetworkDevice, PowerDistributionUnit, Server
+from .serializers import NetworkDeviceDetailSerializer, NetworkDeviceListSerializer,\
+    PowerDistributionUnitDetailSerializer, PowerDistributionUnitListSerializer,\
+    ServerDetailSerializer, ServerListSerializer
 
 
 class ServerViewSet(viewsets.ModelViewSet):
@@ -13,4 +15,26 @@ class ServerViewSet(viewsets.ModelViewSet):
         queryset = Server.objects.all()
         context = {'request': request}
         serializer = ServerListSerializer(queryset, many=True, context=context)
+        return Response(serializer.data)
+
+
+class NetworkDeviceViewSet(viewsets.ModelViewSet):
+    queryset = NetworkDevice.objects.all()
+    serializer_class = NetworkDeviceDetailSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = NetworkDevice.objects.all()
+        context = {'request': request}
+        serializer = NetworkDeviceListSerializer(queryset, many=True, context=context)
+        return Response(serializer.data)
+
+
+class PowerDistributionUnitViewSet(viewsets.ModelViewSet):
+    queryset = PowerDistributionUnit.objects.all()
+    serializer_class = PowerDistributionUnitDetailSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = PowerDistributionUnit.objects.all()
+        context = {'request': request}
+        serializer = PowerDistributionUnitListSerializer(queryset, many=True, context=context)
         return Response(serializer.data)
