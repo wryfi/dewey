7eab85a489017c2b6d39d80230b9a4192a717a31
Chris Haumesser <chaumesser@plos.org>
2016-12-01 15:44:23 -0800

    ITP-1133 check hostname against environment.hostname_regex

--- modules/environments/admin.py
+++ modules/environments/admin.py
@@ -4,14 +4,16 @@ from django.apps import apps
 from django.contrib import admin
 from django import forms
 
-from .models import Host
+from .models import Environment, Host
 
 
 class CustomHostForm(forms.ModelForm):
-    def clean_hostname(self):
-        if not re.match(r'(.*\.)+.*', self.cleaned_data['hostname']):
-            raise forms.ValidationError('please enter a FULLY QUALIFIED name')
-        return self.cleaned_data['hostname']
+    def clean(self):
+        environment = self.cleaned_data['environment']
+        regex = re.compile(environment.hostname_regex)
+        if not re.match(regex, self.cleaned_data['hostname']):
+            raise forms.ValidationError('that is not a valid hostname for this environment')
+        return self.cleaned_data
 
 
 class HostAdmin(admin.ModelAdmin):
